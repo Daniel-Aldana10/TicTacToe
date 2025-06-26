b988a68c854785bb9b36c2bcd1b02fd40eeed227
Daniel Aldana <daniel.aldana-b@mail.escuelaing.edu.co>
2025-06-26 12:43:00 -0500

    implementation of tic-tac-toe and readme

---
+++ README.md
@@ -0,0 +1,80 @@
+# React Tic Tac Toe
+
+A simple implementation of the classic **Tic Tac Toe** game built with **React** and **Vite**. It supports two players (X and O), tracks move history, determines a winner or a draw, and allows users to jump to previous moves or reset the game.
+
+---
+
+## Getting Started
+
+Follow these steps to run the project locally in development mode.
+
+### Prerequisites
+
+Make sure you have the following installed:
+
+- [Node.js 16+](https://nodejs.org/)
+- [npm 8+](https://www.npmjs.com/get-npm) or [yarn](https://yarnpkg.com/)
+- [Vite](https://vite.dev/)
+- Git (optional, for cloning the repository)
+
+### Installation
+
+Clone the repository:
+
+```bash
+git clone https://github.com/Daniel-Aldana10/TicTacToe
+cd TicTacToe
+
+
+### Installing
+
+Clone the repository:
+
+```bash
+git clone https://github.com/Aldana10/TicTacToe
+cd TicTacToe
+```
+
+Install dependencies:
+```
+npm install
+```
+### Running the Application
+Start the development server with:
+
+```
+npm run dev
+```
+
+### Features
+*  2-player turn-based gameplay (X and O)
+
+*  Winner detection logic
+
+*  Tie/draw detection
+
+* Move history with ability to jump to any previous state
+
+* Functional components using React Hooks
+
+*  Game reset functionality
+
+### Folder Structure
+```
+react-tic-tac-toe/
+├── public/
+│   └── index.html
+├── src/
+│   ├── App.js          # Main game logic (Game, Board, Square)
+|   ├── Game.jsx 
+│   └── index.js        # Entry point
+├── package.json
+└── README.md
+```
+### Built With
+* React – UI library
+* JavaScript (ES6+)
+
+## Authors
+
+* **Daniel Aldana** - [GitHub](https://github.com/Daniel-Aldana10)
---
+++ src/Game.jsx
@@ -0,0 +1,130 @@
+import { useState } from 'react';
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const [movements, setMovements] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    if (nextMove === 0) {
+      setMovements(0);
+    }
+    setCurrentMove(nextMove);
+  }
+
+  function handleReset() {
+    setHistory([Array(9).fill(null)]);
+    setCurrentMove(0);
+    setMovements(0);
+  }
+
+  const moves = history.map((squares, move) => {
+    const description = move > 0 ? 'Movimiento#' + move : 'Inicio';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          onPlay={handlePlay}
+          movements={movements}
+          setMovements={setMovements}
+        />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+      <footer>
+        <button onClick={handleReset}>Reiniciar Juego</button>
+      </footer>
+    </div>
+  );
+}
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay, movements, setMovements }) {
+    
+  function handleClick(i) {
+    if (movements === 9 || calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    setMovements(movements + 1);
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    onPlay(nextSquares);
+  }
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+const winner = calculateWinner(squares);
+let status;
+if (winner) {
+  status = 'Ganador: ' + winner;
+} else {
+  status = movements < 9 ? 'Turno jugador: ' + (xIsNext ? 'X' : 'O'): "Empate";
+}
+
+return (
+  <>
+    <div className="status">{status}</div>
+    <div className="board-row">
+      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+    </div>
+    <div className="board-row">
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+    </div>
+    <div className="board-row">
+      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+    </div>
+  </>
+);
+}
